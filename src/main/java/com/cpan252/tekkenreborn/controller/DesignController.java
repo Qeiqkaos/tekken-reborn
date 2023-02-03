b4eed36c2be6106699c29f4cd1ba5dc25236d358
Qeiqkaos <subanov.oleg@mail.ru>
2023-02-02 22:32:13 -0500

    Lab 2

---
+++ src/main/java/com/cpan252/tekkenreborn/controller/DesignController.java
@@ -0,0 +1,64 @@
+package com.cpan252.tekkenreborn.controller;
+
+import java.util.EnumSet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.cpan252.tekkenreborn.model.Fighter;
+import com.cpan252.tekkenreborn.model.Fighter.Anime;
+import com.cpan252.tekkenreborn.model.FighterPool;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/design")
+@SessionAttributes("fighterPool")
+public class DesignController {
+    @GetMapping
+    public String design() {
+        return "design";
+    }
+
+
+    @ModelAttribute
+    public void animes(Model model) {
+        var animes = EnumSet.allOf(Anime.class);
+        model.addAttribute("animes", animes);
+        log.info("animes converted to string:  {}", animes);
+    }
+
+
+    @ModelAttribute(name = "fighterPool")
+    public FighterPool fighterPool() {
+        return new FighterPool();
+    }
+
+
+    @ModelAttribute
+    public Fighter fighter() {
+        return Fighter
+                .builder()
+                .build();
+    }
+
+
+    @PostMapping
+    public String processFighterAddition(@Valid Fighter fighter,
+            @ModelAttribute FighterPool pool, BindingResult result) {
+        if (result.hasErrors()) {
+            return "design";
+        }
+        pool.add(fighter);
+        return "redirect:/design";
+    }
+}
