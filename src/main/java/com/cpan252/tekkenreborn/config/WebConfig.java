d9aac9137eaf5165920da02a21cf4ff005332602
Qeiqkaos <subanov.oleg@mail.ru>
2023-04-19 15:06:08 -0400

    Lab 09

--- src/main/java/com/cpan252/tekkenreborn/config/WebConfig.java
+++ src/main/java/com/cpan252/tekkenreborn/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.cpan252.tekkenreborn.config;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/login");
         registry.addViewController("/").setViewName("design.html");
         registry.addViewController("/").setViewName("authentication/login");
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
